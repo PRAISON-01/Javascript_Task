b9633466d886f75068afc66d9fdbf37a2b41a976
Praise Nwankwo <nwankwopraison@gmail.com>
2026-05-08 08:31:28 +0100

    Changed

--- Assignment/EasterEggs/ChinChin/TemperatureChecker.js
+++ Assignment/EasterEggs/ChinChin/TemperatureChecker.js
@@ -1,19 +1,39 @@
 const input = require("prompt-sync")();
 
 const temp = Number(input("Ente a temperature value: "));
+let degree;
 
-switch (true){
-
-    case(temp < 0): 
-        console.log("Freezing");
-        break;
-    case(temp > 0):
-        console.log("Cold");
-        break;
-    case(temp > 15):
-        console.log("Warm");
-        break;
-    case(temp > 25):
-        console.log("High temperature");
-        break;
+if(temp <= 0){
+    degree = "Freezing";
 }
+else if(temp < 15){
+        degree = "Cold";
+}
+
+else if(temp < 25){
+    degree = "Warm";
+}
+else{
+    degree = "High temperature";
+}
+
+console.log(degree);
+
+//switch (false){
+//    case(temp <= 0): 
+//        degree = "Freezing";
+//        break;
+//    case(temp < 15):
+//        degree = "cold";
+//        break;
+//    case(temp < 25):
+//        degree = "warm";
+//        break;
+//    case(temp > 25):
+//       degree = "hot";
+//       break;
+//    default:
+//        console.log("invalid temp");
+//}
+//
+//console.log(degree);
